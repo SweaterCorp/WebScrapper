df4f9856b267efc1d0a3713b62147fca0fd60ec8
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-07 21:23:11 +0300

    Исправил опечатки

--- WebScrapper/WebScrapper.py
+++ WebScrapper/WebScrapper.py
@@ -5,6 +5,7 @@ import csv
 import urllib.request
 import typing 
 from typing import List
+import re
 
 def parse_types(html:str, class_attribute: str):
     results = []
@@ -17,16 +18,18 @@ def parse_types(html:str, class_attribute: str):
         results.append(text)
     return results
 
-def parse_clothes(html:str):
-    results = []
-    soup = BeautifulSoup(html, "html.parser")
-    table_left_column = soup.find("div", {"class": "table__left-column"})
-    list = table_left_column.find("div", {"class": "catalog-navigation-wrap"}).find("ul").find("ul")
-    items = list.find_all("a", {"class":"link"})
-    for item in items:
-        text = item.get_text()
-        href = item["href"]
-        results.append((text, href))
+def parse_clothes_list(html:str):
+    results = [];
+    soup = BeautifulSoup(html, "html.parser");
+    table_left_column = soup.find("div", {"class": "table__left-column"});
+    list = table_left_column.find("div", {"class": "catalog-navigation-wrap"}).find("ul").find("ul").find_all("li");
+    for item in list:
+        count = item.find("span", {"class":"cat-nav-cnt"}).get_text();
+        link = item.find("a", {"class":"link"})
+        text = link.get_text().strip().replace('\n', '')
+        text = re.sub(r'\s+', ' ', text)
+        href = link["href"]
+        results.append((text, href, count))
     return results
 
 def read_html(file_path:str):
@@ -53,7 +56,18 @@ def clothes_to_csv_lines(rows, delimeter = ";"):
         results.append(line);
     return results;
 
+#https://www.lamoda.ru/c/399/clothes-bluzy-rubashki/?page=2
 
+# 60 на странице вещей
+
+
+#def parse_clothes_item(html:str):
+
+
+    #http://a.lmcdn.ru/product/O/V/OV001EWBRUQ2_6867434_1_v2.jpg
+    #http://a.lmcdn.ru/img46x66/O/V/OV001EWBRUQ2_6867434_1_v2.jpg
+    #
+    
 
 def main():
     sites_folder = "../ParsingSites/"
@@ -74,7 +88,7 @@ def main():
     sizes = parse_types(html_text, size_types)
     colors = parse_types(html_text, color_types)
 
-    clothes = parse_clothes(html_text)
+    clothes = parse_clothes_list(html_text)
 
 
     write_csv(data_folder + "Prints.csv", types_to_csv_lines(prints));
@@ -87,13 +101,27 @@ def main():
 
 
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
 
-url = "https://www.lamoda.ru/p/ov001ewbruq2/clothes-ovs-dzhinsy/";
-html = requests.get(url).text;#urllib.request.urlopen(url).read();
-with open("../ParsingSites/Model.html", "w",  encoding="utf8", newline="") as file:
-    file.write(html)
+#url = "https://www.lamoda.ru/p/ov001ewbruq2/clothes-ovs-dzhinsy/";
+#html = requests.get(url).text;#urllib.request.urlopen(url).read();
+#with open("../ParsingSites/Model.html", "w",  encoding="utf8", newline="") as file:
+#    file.write(html)
 
 #a = parse_types(htmlText, brands_class)
 #print(a)
+
+rubashki="https://www.lamoda.ru/c/399/clothes-bluzy-rubashki/"
+bryuki="https://www.lamoda.ru/c/401/clothes-bryuki-shorty-kombinezony/"
+verkhnyaya="https://www.lamoda.ru/c/357/clothes-verkhnyaya-odezhda/"
+djemperi="https://www.lamoda.ru/c/371/clothes-trikotazh/"
+djinsy="https://www.lamoda.ru/c/397/clothes-d-insy/"
+pidzhaki="https://www.lamoda.ru/c/367/clothes-pidzhaki-zhaketi/"
+platiya="https://www.lamoda.ru/c/369/clothes-platiya/"
+kostyumy="https://www.lamoda.ru/c/415/clothes-kostyumy/"
+olimpiyki="https://www.lamoda.ru/c/2474/clothes-tolstovki-olimpiyki/"
+topy="https://www.lamoda.ru/c/2627/clothes-topy/"
+futbolki="https://www.lamoda.ru/c/2478/clothes-futbolki/"
+shorty="https://www.lamoda.ru/c/2485/clothes-shorty/"
+yubki="https://www.lamoda.ru/c/423/clothes-yubki/"
